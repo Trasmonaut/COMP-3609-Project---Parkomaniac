61068a86673a20c7905c80587a0473528522d04b
Caleb <c.ramdath.26@gmail.com>
2025-03-22 21:19:30 -0400

    szdxghjkl;'
    '

---
+++ player.java
@@ -0,0 +1,11 @@
+public class player {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    public boolean isStunned;
+    private int stunDuration;
+    
+    
+}
