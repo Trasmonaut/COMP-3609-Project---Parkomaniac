d3c83d39566ea3db34439f10d2549fbd00c7b004
Caleb <c.ramdath.26@gmail.com>
2025-03-23 20:01:21 -0400

    Added basic player images. more to add still

--- player.java
+++ player.java
@@ -4,8 +4,14 @@ public class player {
     private int y;
     private int width;
     private int height;
-    public boolean isStunned;
-    private int stunDuration;
+    public boolean isStunned;    // if player is stunned then 1, else 0. 
+    private int stunDuration;    //player stays stunned for x seconds
+    private boolean facingDirection;  // 1 = right, 0 = left
+    private boolean gravityDirection;   // 1 = regular 0 = up
+    
+    public static void main(String[] args) {
+        
+    }
     
     
 }
